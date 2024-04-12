75c925abb6afe4657d42d0cc7a7a7c2f1009fef6
sbmelb <stephen.bick@gmail.com>
2024-04-12 16:20:29 +1000

    Update keymap.c

--- keyboards/converter/usb_usb/keymaps/default/keymap.c
+++ keyboards/converter/usb_usb/keymaps/default/keymap.c
@@ -41,7 +41,9 @@ enum custom_keycodes {
     UNDO,
     REDO,
     L_INDENT,
-    R_INDENT
+    R_INDENT,
+    DOT_LIST,
+    DASH_LIST
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -316,7 +318,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
 
 
+        case DOT_LIST:
+        if (record->event.pressed) {
+            // increase indent
+            SEND_STRING(SS_LALT(SS_TAP(X_F19)));
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
 
+        case DASH_LIST:
+        if (record->event.pressed) {
+            // increase indent
+            SEND_STRING(SS_LALT(SS_TAP(X_F18)));
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
 
     }
     return true;
@@ -346,13 +364,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------' `-----------' `---------------' `-------'
      */
     [0] = LAYOUT_all(
-                         KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,    KC_F20,     KC_F21,    KC_F22,    KC_F23,    KC_F24,
-    KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,     KC_F8,      KC_F9,     KC_F10,    KC_F11,    KC_F12,                     QK_RBT,    KC_SCRL,   QK_BOOT,         KC_VOLD,   KC_VOLU,   KC_MUTE,      KC_PWR,     KC_HELP,
-    KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     ONE_PT,   THRE_PT,  SIX_PT,    EGHT_PT,    TWLV_PT,   EGTN_PT,   TWFR_PT,   KC_INT3,   KC_BSPC,         KC_INS,    KC_HOME,   KC_PGUP,         KC_NUM,    KC_PSLS,   KC_PAST,      KC_PMNS,    KC_STOP, KC_AGIN,
-    QUOTES,    CAPS,     KC_W,     KC_E,     UND,      KC_T,     KC_Y,     KC_U,     KC_I,      KC_O,       KC_P,      RD_BKT,    SQ_BKT,               KC_BSLS,         KC_DEL,    KC_END,    KC_PGDN,         KC_P7,     KC_P8,     KC_P9,        KC_PPLS,    KC_MENU, KC_UNDO,
-    KC_CAPS,   CLEAR,    BOLD,     ITALIC,   HLGHT,    KC_G,     KC_H,     KC_J,     KC_K,      KC_L,       KC_SCLN,   KC_QUOT,              KC_NUHS,   KC_ENT,                                                 KC_P4,     KC_P5,     KC_P6,        KC_PCMM,    KC_SLCT, KC_COPY,
-    KC_LSFT,   KC_NUBS,  COPY,     SF_PST,   PASTE,    KC_V,     KC_B,     KC_N,     KC_M,      L_INDENT,   R_INDENT,  KC_SLSH,              KC_INT1,   KC_RSFT,                    KC_UP,                      KC_P1,     KC_P2,     KC_P3,        KC_PEQL,    KC_EXEC, KC_PSTE,
-    KC_LCTL,   UNDO,     REDO,     KC_INT5,  KC_LNG2,            KC_SPC,             KC_NO,     KC_NO,      KC_INT2,   KC_NO,     KC_NO,     KC_NO,     KC_NO,           KC_LEFT,   KC_DOWN,   KC_RGHT,         KC_P0,              KC_PDOT,        KC_PENT,    KC_FIND, KC_CUT
+                         KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,     KC_F19,    KC_F20,     KC_F21,    KC_F22,    KC_F23,    KC_F24,
+    KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,      KC_F7,     KC_F8,      KC_F9,     KC_F10,    KC_F11,    KC_F12,                     QK_RBT,    KC_SCRL,   QK_BOOT,         KC_VOLD,   KC_VOLU,   KC_MUTE,      KC_PWR,     KC_HELP,
+    KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     ONE_PT,   THRE_PT,    SIX_PT,    EGHT_PT,    TWLV_PT,   EGTN_PT,   TWFR_PT,   KC_INT3,   KC_BSPC,         KC_INS,    KC_HOME,   KC_PGUP,         KC_NUM,    KC_PSLS,   KC_PAST,      KC_PMNS,    KC_STOP, KC_AGIN,
+    QUOTES,    CAPS,     KC_W,     KC_E,     UND,      KC_T,     KC_Y,     DASH_LIST,  DOT_LIST,      KC_O,       KC_P,      RD_BKT,    SQ_BKT,               KC_BSLS,         KC_DEL,    KC_END,    KC_PGDN,         KC_P7,     KC_P8,     KC_P9,        KC_PPLS,    KC_MENU, KC_UNDO,
+    KC_CAPS,   CLEAR,    BOLD,     ITALIC,   HLGHT,    KC_G,     KC_H,     KC_J,       KC_K,      KC_L,       KC_SCLN,   KC_QUOT,              KC_NUHS,   KC_ENT,                                                 KC_P4,     KC_P5,     KC_P6,        KC_PCMM,    KC_SLCT, KC_COPY,
+    KC_LSFT,   KC_NUBS,  COPY,     SF_PST,   PASTE,    KC_V,     KC_B,     KC_N,       KC_M,      L_INDENT,   R_INDENT,  KC_SLSH,              KC_INT1,   KC_RSFT,                    KC_UP,                      KC_P1,     KC_P2,     KC_P3,        KC_PEQL,    KC_EXEC, KC_PSTE,
+    KC_LCTL,   UNDO,     REDO,     KC_INT5,  KC_LNG2,            KC_SPC,               KC_NO,     KC_NO,      KC_INT2,   KC_NO,     KC_NO,     KC_NO,     KC_NO,           KC_LEFT,   KC_DOWN,   KC_RGHT,         KC_P0,              KC_PDOT,        KC_PENT,    KC_FIND, KC_CUT
     ),
     [1] = LAYOUT_all(
                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
